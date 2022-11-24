b3a99b4668ff63ae662f868af3da773fd9ee47ce
Jeferson Domingos <jefersondomingos77@gmail.com>
2022-11-24 01:42:19 -0300

    iniciando logica

--- main.js
+++ main.js
@@ -6,6 +6,25 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+//CRUD : create, read, update, delete.
+
+const tempClient = {
+    nome: 'teste',
+    email: 'teste@email.com',
+    celular: '31321112123',
+    cidade: 'Urucânia-MG',
+    loginPPPoE: 'jefersondmartins',
+    id: '5',
+    plano: '300mb'
+}
+
+const createClient = (client) => {
+    const db_client = JSON.parse(localStorage.getItem('db_client'));
+    db_client.push(client);
+    localStorage.setItem('db_client', JSON.stringify(db_client));
+}
+
+//Eventos
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
