c034ec176d4e7eecef73a6fd7f0cf2af00495c79
Jeferson Domingos <jefersondomingos77@gmail.com>
2022-11-25 23:12:26 -0300

    criando cliete pelo formulário

--- main.js
+++ main.js
@@ -45,9 +45,41 @@ const deleteClient = (index) => {
     setLocalStorage(dbClient);
 }
 
+//checando campos
+const isValidFields = () => {
+    return document.getElementById('form').reportValidity();
+}
+
+//interação com o usuário
+const clearFields = () => {
+    const fields = document.querySelectorAll('.modal-field');
+    fields.forEach(field => field.value = "");
+}
+
+const saveClient = () => {
+    if(isValidFields()){
+        const client = {
+           nome: document.getElementById('nome').value,
+           email: document.getElementById('email').value,
+           celular: document.getElementById('celular').value,
+           cidade: document.getElementById('cidade').value,
+           loginPPPoE: document.getElementById('login').value,
+           id: document.getElementById('id').value,
+           plano: document.getElementById('plano').value
+        }
+        createClient(client);
+        clearFields();
+        closeModal();
+        console.log('cadastrado com sucesso');
+    }
+}
+
 //Eventos
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
 document.getElementById('modalClose')
     .addEventListener('click', closeModal)
+
+document.getElementById('salvar')
+    .addEventListener('click', saveClient);
